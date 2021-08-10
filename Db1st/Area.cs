958d5f771977de2ea0dfbb8ca4b73ee023c0667e
YL <simon-ling@hotmail.com>
2021-08-09 23:02:56 -0400

    02-0000 done

--- Db1st/Area.cs
+++ Db1st/Area.cs
@@ -17,7 +17,7 @@ namespace Db1st
         public int AreaId { get; set; }
         public string AreaName { get; set; }
         public int CityId { get; set; }
-        public byte[] Pincode { get; set; }
+        public string Pincode { get; set; }
     
         public virtual City City { get; set; }
     }
--- Db1st/Location.Designer.cs
+++ Db1st/Location.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\YL\Desktop\Tutorials\C#\EF6\CodeSemantic\Db1st\Db1st\Location.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\YL\Desktop\Tutorials\C#\EF6\CodeSemantic\2\Db1st\Db1st\Location.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
