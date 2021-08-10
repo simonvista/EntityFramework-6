418ff5ea70894292b4d46dc65b823024cd936879
YL <simon-ling@hotmail.com>
2021-08-10 13:11:39 -0400

    05-0000 is done

---
+++ Db1st/LazyLoading.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Db1st
+{
+    class LazyLoading
+    {
+        public static void Main(string[] args)
+        {
+            MyDbEntities db = new MyDbEntities();
+            foreach(City city in db.Cities)
+            {
+                Console.WriteLine($"CityId: {city.CityId}, CityName: {city.CityName}");
+                foreach(Area area in city.Areas)
+                {
+                    Console.WriteLine($"AreaId: {area.AreaId}, AreaName: {area.AreaName}, pincode: {area.Pincode}");
+                }
+            }
+        }
+    }
+}
