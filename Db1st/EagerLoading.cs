5b40e25f3416ef94a44dd72020c8589bccb6805d
YL <simon-ling@hotmail.com>
2021-08-10 13:37:11 -0400

    06-0000 is done

---
+++ Db1st/EagerLoading.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Db1st
+{
+    class EagerLoading
+    {
+        public static void Main(string[] args)
+        {
+            MyDbEntities db = new MyDbEntities();
+            // Include("Areas") -> eager loading
+            foreach (City city in db.Cities.Include("Areas"))
+            {
+                Console.WriteLine($"CityId: {city.CityId}, CityName: {city.CityName}");
+                foreach(Area area in city.Areas)
+                {
+                    Console.WriteLine($"AreaName: {area.AreaName}");
+                }
+            }
+        }
+    }
+}
