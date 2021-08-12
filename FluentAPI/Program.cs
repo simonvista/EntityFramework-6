0fb5f3275820e99256d3860c0b56033a4ee051ce
YL <simon-ling@hotmail.com>
2021-08-12 00:31:33 -0400

    23-0000 is done

--- FluentAPI/Program.cs
+++ FluentAPI/Program.cs
@@ -10,12 +10,22 @@ namespace FluentAPI
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            try
+            {
+                MyDbContext db = new MyDbContext();
+                Department dpt = new Department();
+                dpt.DeptId = 1;
+                dpt.DeptName = "CS";
+                db.Departments.Add(dpt);
+                db.SaveChanges();
+                Console.WriteLine("Department created");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"error: {ex.ToString()}");
+            }
+            //hold the console open
             Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
 }
